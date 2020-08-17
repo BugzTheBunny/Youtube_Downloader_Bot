cc8af0a2fe32ad6e30a1d4b308c62219bc36201a
Slava <urigami2010@gmail.com>
2020-08-17 18:22:01 +0300

    Minor tweaks

--- bot.py
+++ bot.py
@@ -2,8 +2,7 @@ import youtube_dl
 from aiogram import Bot, Dispatcher, executor, types
 import os, time
 
-token = ''  # You need a telegram token, you can create your own bot with this,
-            # just search for a video on how to create a bot on telegram (15 seconds), and input the token here.
+token = ''  # You need a bot token, search for BotFather on telegram.
 bot = Bot(token=token)
 dispatcher = Dispatcher(bot)
 
@@ -15,25 +14,27 @@ async def start(message: types.message):
 
 @dispatcher.message_handler()
 async def start(message: types.message):
+    """
+    Takes the url, sends it to the download func.
+    The download func returns a list of the file.mp3, and the title.
+    The file is sent to the user, and the title is just so we can delete the file later on
+    :param message:
+    :return:
+    """
     await bot.send_message(message.from_user.id, 'Great! Wait a few seconds.. im downloading it for you..')
-    video = await run(message.text)
+    video = await download(message.text)
     await bot.send_message(message.from_user.id, 'Downloaded..sending it to you...')
     mp3 = video[0]
     await bot.send_audio(message.from_user.id, audio=mp3)
+    # Delete the file.
     time.sleep(5)
     os.remove(video[1])
 
 
-async def run(url):
-    video_info = youtube_dl.YoutubeDL().extract_info(
-        url=url, download=False
-    )
+async def download(url):
+    video_info = youtube_dl.YoutubeDL().extract_info(url=url, download=False)
     filename = f"{video_info['title']}.mp3"
-    options = {
-        'format': 'bestaudio/best',
-        'keepvideo': False,
-        'outtmpl': filename
-    }
+    options = {'format': 'bestaudio/best', 'keepvideo': False, 'outtmpl': filename}
 
     with youtube_dl.YoutubeDL(options) as ydl:
         ydl.download([video_info['webpage_url']])
